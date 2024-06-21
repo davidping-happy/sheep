7b1e20e665dbedb7c7fa6685fa9805ba9a937d2b
davidping-happy <breadoflifeadult2@gmail.com>
2024-06-21 14:55:54 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,35 +8,35 @@ var firebaseConfig = {
     messagingSenderId: "86156094842",
     appId: "1:86156094842:web:7c53aaff90ccbd2ed73ac3"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Reference to the database service
 var database = firebase.database();
 
 document.getElementById('meetingForm').addEventListener('submit', submitForm);
 
 function submitForm(e) {
     e.preventDefault();
+    
+    var group = document.getElementById('group').value;
+    var date = document.getElementById('date').value;
+    var number = document.getElementById('number').value;
 
-    // Get values
-    var group = getInputVal('group');
-    var date = getInputVal('date');
-    var number = getInputVal('number');
-
-    // Save meeting data
     saveMeeting(group, date, number);
-
+    
+    // Show confirmation message
+    document.getElementById('confirmationMessage').style.display = 'block';
+    
+    // Hide confirmation message after 3 seconds
+    setTimeout(function() {
+        document.getElementById('confirmationMessage').style.display = 'none';
+    }, 3000);
+    
     // Clear form
     document.getElementById('meetingForm').reset();
 }
 
-// Function to get form values
-function getInputVal(id) {
-    return document.getElementById(id).value;
-}
-
-// Save meeting data to firebase
 function saveMeeting(group, date, number) {
     var newMeetingRef = database.ref('meetings').push();
     newMeetingRef.set({
